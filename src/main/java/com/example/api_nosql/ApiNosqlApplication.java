ca38f6be89b379a61fd3c2f5cff122eb5ca5d64a
Pedro Gabriel <pedro.correia@germinare.org.br>
2025-09-15 20:36:03 -0300

    chore: setup Spring Boot project with base configuration and dependencies

---
+++ src/main/java/com/example/api_nosql/ApiNosqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNosqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNosqlApplication.class, args);
+	}
+
+}
